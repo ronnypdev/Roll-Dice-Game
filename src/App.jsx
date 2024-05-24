442e89dbcb03d33475b89d20ee7ab8c6d9db8966
Ronny Perez <ronnypdev@gmail.com>
2024-05-24 15:39:21 -0400

    Implemented the handleRollDice function and code refactor

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,51 @@ import Die from "./Die"
 import './App.css'
 
 function App() {
-  const [dieNumbers, setDieNumbers] = useState(allNewDice())
-
+  /**
+   *  Create state to hold our array of numbers. (Initialize
+   * the state by calling our `allNewDice` function so it
+   * loads all new dice as soon as the app loads)
+  */
+  const [dice, setDice] = useState(allNewDice())
+  /**
+   * allNewDice() function
+   * which generates number from 1 to 6
+   * inclusive
+   * returns an array
+   * of 10 random numbers between 1-6 inclusive.
+   * @returns newDice
+  */
   function allNewDice() {
-      const newDice = []
-      for (let i = 0; i < 10; i++) {
-          newDice.push(Math.ceil(Math.random() * 6))
-      }
-      return newDice
+    const newDice = []
+    for (let i = 0; i < 10; i++) {
+        newDice.push(Math.ceil(Math.random() * 6))
+    }
+    return newDice
   }
-
-  const dieElement = dieNumbers.map((die) => {
+  /**
+   * handleDiceRoll() function
+   * which triggers the dice
+   * to re render every time the roll dice
+   * button is clicked
+  */
+  function handleDiceRoll() {
+    setDice(allNewDice())
+  }
+  /**
+   * This is the diceElement
+   * in order to render inside the
+   * dice container
+   */
+  const diceElement = dice.map((die) => {
      return <Die key={die.index} value={ die} />
   })
 
-
-
   return (
     <main>
-        <div className="dice-container">
-          {dieElement}
-        </div>
+      <div className="dice-container">
+        {diceElement}
+      </div>
+      <button className="roll-dice" onClick={handleDiceRoll}>Roll</button>
     </main>
   )
 }
