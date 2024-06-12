bb430e7868c4e6c12f67fe4e374be691034d1737
Ronny Perez <ronnypdev@gmail.com>
2024-06-12 16:05:30 -0400

    Implemented and refactor last function for dice hold

--- src/Die.jsx
+++ src/Die.jsx
@@ -4,16 +4,27 @@ export default function Die(props) {
     backgroundColor: props.isHeld ? "#59E391" : "#ffffff"
   }
 
+  /**
+   * diceNume is the array
+   * that holds all the values being
+   * passed down to the die prop
+   * which allows to render the random numbers
+   * of dice dots
+   */
   const diceNum = []
   for (let i = 0; i < props.value; i++) {
     diceNum.push(props.value)
   }
 
+  /**
+   *
+   * Generates unique keys when mapping over an array
+   * @returns
+   */
   const generateKey = (item, index) => `${item}-${index}`;
 
   return (
       <div className="die-face" style={dieColor} onClick={props.holdDice}>
-        {/* <h2 className="die-num">{props.value}</h2> */}
         {diceNum.map((dieNum, index) =>
           <span key={generateKey(dieNum, index)} className="pip"></span>
         )}
